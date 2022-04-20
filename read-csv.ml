7b8a32cebc2963b90448dc0d7aba84befca23314
Gabriel Aragão <gabriel.aragao.comp@gmail.com>
2022-04-20 10:09:05 +0100

    feat: print recipe names

--- read-csv.ml
+++ read-csv.ml
@@ -47,30 +47,55 @@ let iterate_line_by_line lines =
 let create_recipe_matrix lines = 
   let lines_length = List.length lines in
   let columns_length = List.length (String.split_on_char '\t' (List.nth lines 1)) in
-  let recipes_ingredients_matrix = Array.make_matrix lines_length columns_length "" in   
+  let recipes_matrix = Array.make_matrix lines_length columns_length "" in   
   for i = 0 to lines_length - 1 do 
     let columns = String.split_on_char '\t' (List.nth lines i) in
     (* Get all lines and split as columns when element is found *)
       for j = 0 to columns_length - 1 do
         (* Get jth column and put it on the list according to line (i) and column (j) *)
         let column_nth = List.nth columns j in 
-        recipes_ingredients_matrix.(i).(j) <- column_nth
+        recipes_matrix.(i).(j) <- column_nth
       done
   done;
-  recipes_ingredients_matrix
+  recipes_matrix
 
 
-let print_ingredients_for_recipe recipe_number ingredients_matrix =
+let print_ingredients_for_recipe recipe_number recipes_matrix =
   print_endline "Ingredientes: ";
+  (* Getting only the ingredients *)
   for i = 3 to 24 do
-    print_string ingredients_matrix.(recipe_number).(i); print_string " ";
+    print_string recipes_matrix.(recipe_number).(i); print_string " ";
   done;
   
-  
+
+let print_recipe_title recipe_number recipes_matrix =
+  print_string ("Nome: " ^ recipes_matrix.(recipe_number).(1));
+
+let print_recipe_preparation recipe_number recipes_matrix = 
+  print_endline "Preparo: "
+  print_string recipes_matrix.(recipe_number).(1);
+
+let rec print_all_recipes_titles recipes_matrix line_number = 
+  match line_number with
+  | _ when line_number >= Array.length recipes_matrix -> ()
+  | _ -> 
+    print_recipe_title line_number recipes_matrix;
+    print_all_recipes_titles recipes_matrix (line_number+1)
+    
+
+let rec print_n_recipes_details recipe_number recipes_matrix remaining_recipes =
+  match remaining_recipes with
+  0 -> ()
+  | -> 
+    print_endline "Escolha 3 receitas das citadas."; let input_by_user = read_int() in
+    print_ingredients_for_recipe input_by_user matrix_of_ingredients;
+    read_input input_by_user in read_input 9;
+
+    
 
 
 (* While user input is not 0 keep asking for text *)
-let () = 
+(* let () = 
   let lines_read = lines_in_file file_receitas in
   let matrix_of_ingredients = create_recipe_matrix lines_read in 
   let rec read_input input  =
@@ -80,3 +105,10 @@ let () =
       print_endline "Diz ai receita que tu quer po. Se quiser parar manda um -1 pra nois"; let input_by_user = read_int() in
       print_ingredients_for_recipe input_by_user matrix_of_ingredients;
       read_input input_by_user in read_input 9;
+ *)
+
+
+let () = 
+  let lines_read = lines_in_file file_receitas in
+  let matrix_of_ingredients = create_recipe_matrix lines_read in 
+  print_all_recipes_titles matrix_of_ingredients 1
